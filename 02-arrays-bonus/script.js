779c647d019ac7c302a4eff4447f23231e74d547
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-19 22:07:08 +0100

    [UPD] Quarta task bonus completata

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -17,20 +17,29 @@ console.log(reversedTeachers);
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = [];
 
-for(let i=0; i<teachers.length; i++ ){
+for (let i = 0; i < teachers.length; i++) {
 
-  if(teachers[i].length >= 5){
+  if (teachers[i].length >= 5) {
     longNames.push(teachers[i])
   }
 }
- console.log(longNames);
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 teachers.splice(5, 1);
 console.log(teachers)
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = [];
+
+for (let i = 0; i < teachers.length; i++) {
+
+  if (teachers[i] == 'Fabio') {
+    isFabioPresent.push(teachers[i]);
+  }
+}
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
